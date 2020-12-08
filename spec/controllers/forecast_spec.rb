f0e565d4b1e43badf274c67b6823529fe0d1457f
Damion Gomez <damiong28@gmail.com>
2020-12-07 20:54:17 -0700

    check for keys of forecast

--- spec/controllers/forecast_spec.rb
+++ spec/controllers/forecast_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe ForecastsController, type: :controller do
   describe 'POST get_forecast' do
     it 'creates a valid forecast' do
       post :get_forecast, params: { zipcode: 12345}
+      forecast = JSON.parse(response.body)["forecast"]
+      expect(forecast).to have_key("zipcode")
+      expect(forecast).to have_key("current_temp")
+      expect(forecast).to have_key("low")
+      expect(forecast).to have_key("high")
+      expect(forecast).to have_key("average")
       expect(response.status).to eq(200)
     end
 
